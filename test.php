559bf74ef00ac48acf1e715acb693ed4f9d0d8fc
root <root@Anonymous>
2023-06-13 15:04:45 +0530

    php project

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test php file</title>
+</head>
+<body>
+	<center>
+		<h1>Test webpage</h1>
+		<?php
+			echo "<h1>php runned sucessfully...</h1>";
+		?>
+	</center>
+</body>
+</html>
